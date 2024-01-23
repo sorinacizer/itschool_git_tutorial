a2adfed1300c5093b006ccc0212b5fa7f67f00ec
cizer_sorina <sorina.cizer@gmail.com>
2024-01-23 14:02:00 +0200

    Second commit - new file added

---
+++ exercise_python.py
@@ -0,0 +1,4 @@
+'''Verifica daca un text introdus de la tastatura contine spatiu. Daca da, afiseaza pe ecran True, altfel
+afiseaza pe ecran False.'''
+def space_in_text(string: str) -> bool:
+    if s
